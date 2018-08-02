04919ea1b8aa04dabeb09aa6eef7755696363172
JCamp-12 <JChandlerCampbell@gmail.com>
2018-08-02 00:37:12 -0400

    starting react through webpack hoping to get everything running through flask soon

---
+++ full_stack_template/static/src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+// import logo from '../images/logo.svg';
+
+// import './App.css';
+
+
+
+
+
+class App extends Component {
+  // state = {
+  //   name: 'Jason'
+  // }
+
+ 
+  
+
+  render() {
+   
+
+
+    return (
+      <div className="App">
+        <h1>testing!!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
